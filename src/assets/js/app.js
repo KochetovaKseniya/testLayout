52157436bdc0857a206e4f6adb2e80a1fbe8086f
KochetovaKseniya <kseniyakochetova01@gmail.com>
2022-05-16 12:23:37 +0300

    commit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,7 +4,22 @@ $(window).ready(() => {
         $(".overlay").fadeOut(500, function () {
             document.dispatchEvent(overlayEvent);
         });
-        autosize($('textarea'));
     }, 300);
 
+    var mailv = 0;
+    var mail = $('[name="mail"]');
+    var pattern = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    mail.bind("change paste keyup", function () {
+        if (mail.val() != '') {
+            if (mail.val().search(pattern) == 0) {
+                $(mail).removeClass('error');
+                return mailv = 1;
+            } else {
+                $(mail).addClass('error');
+                return mailv = 0;
+            }
+        } else {
+            return mailv = 0;
+        }
+    });
 });
